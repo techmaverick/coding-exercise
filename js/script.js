50ff90f1f652cf5b7dd43ab96ea74053b5306153
techmaverick <chrisarmstrong.tx@gmail.com>
2014-03-17 17:08:50 -0500

    Completed JS file with poller API interaction - re-factored the leaderboard DIV from prototype HTML into JS file for dynamic building and insertion based on poller updates

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Created by Chris Armstrong
+//MassRelevance Front-End Dev Exercise
+
+$(function() {
+  //DOM ready;
+  //Check leaders every 15 seconds and limit to the top 5 bands per requirements
+  var options = {frequency: 15, limit: 5};
+  //New massrel Poller with callback function set
+  var bandList = new massrel.Poller(options, function(awesomeBands) {
+  	var htmlContent = '';
+  	//load HTML into leaderboard div with each top band's name and mention count
+  	awesomeBands.forEach(function(band) {
+  		htmlContent+= '<div class="band-details"><div class="band-name">' + band.name + 
+  		'</div><div class="mention-count"><span class="highlight">' + band.count + 
+  		'</span> <span class="mentions">Mentions</span></div><div class="clear-float"></div></div>';
+  	});
+  	//Hide first, so animation is possible
+  	$('.leaderboard').hide();
+  	//Bonus = Animation on each loading of the Leaderboard
+  	$('.leaderboard').html(htmlContent).fadeToggle(1000);
+  });
+  
+  //now start the Band Leaderboard / Mentions Poller
+  bandList.start();
+
+});
